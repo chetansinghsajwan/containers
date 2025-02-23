dd9cadf19076d630d26c8d75dfcd72057968ee31
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2025-02-23 15:05:46 +0530

    update: add cmake_toolchains

---
+++ cmake_toolchains/toolchains/toolchain-linux-amd64.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_NAME "Linux")
+
+set(CMAKE_C_COMPILER "/usr/bin/clang-18")
+set(CMAKE_CXX_COMPILER "/usr/bin/clang++-18")
+set(CMAKE_RC_COMPILER "/usr/bin/llvm-rc-18")
+
+set(CMAKE_FIND_ROOT_PATH "/usr/local/linux-amd64")
---
+++ cmake_toolchains/toolchains/toolchain-win-aarch64.cmake
@@ -0,0 +1,19 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_VERSION 10)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(CMAKE_CROSSCOMPILING TRUE)
+
+set(triple "aarch64-w64-mingw32")
+
+set(CMAKE_C_COMPILER "$ENV{TOOLCHAIN_ROOT}/bin/${triple}-clang")
+set(CMAKE_CXX_COMPILER "$ENV{TOOLCHAIN_ROOT}/bin/${triple}-clang++")
+set(CMAKE_RC_COMPILER "$ENV{TOOLCHAIN_ROOT}/bin/${triple}-windres")
+set(CMAKE_SYSROOT $ENV{TOOLCHAIN_ROOT}/${triple})
+
+# Search programs in the host environment and headers and libraries in the
+# target environment
+set(CMAKE_FIND_ROOT_PATH "/usr/local/win-aarch64")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
